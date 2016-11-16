442bad971b7785c16e434e69421e1e0a9990b5ea
Pat <patbolbox-jet1@yahoo.com>
2016-11-16 17:44:58 +0100

    temp map

--- src/components/map/map.ts
+++ src/components/map/map.ts
@@ -8,15 +8,22 @@ import { Component } from '@angular/core';
 */
 @Component({
   selector: 'map',
-  templateUrl: 'map.html'
+  templateUrl: '<div id="map_aroud" height="100%" width="100%"></div>'
 })
 export class MapComponent {
+  map: google.maps.Map;
 
-  text: string;
+  public init(lat: number, long: number, zoom: number=20) {
+    this.map = new google.maps.Map(document.getElementById('map_around'), {
+      center: new google.maps.LatLng(lat, long),
+      zoom: zoom,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+    });
+  }
 
   constructor() {
     console.log('Hello Map Component');
-    this.text = 'Hello World';
   }
 
 }
+
